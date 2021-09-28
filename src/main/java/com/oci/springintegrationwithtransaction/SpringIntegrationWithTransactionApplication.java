9fcf95ab2a8fd8e111e6723de5d12c0805dad07a
Karlos Chita <karloslcp@gmail.com>
2021-09-27 21:24:55 -0500

    Initial commit

---
+++ src/main/java/com/oci/springintegrationwithtransaction/SpringIntegrationWithTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.oci.springintegrationwithtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationWithTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationWithTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oci/springintegrationwithtransaction/SpringIntegrationWithTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci.springintegrationwithtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationWithTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
